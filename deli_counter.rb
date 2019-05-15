a600f001663582bdc05d6a315e162a0bbcc595b8
Heeraj Ramsanai <ramsanai_heeraj@live.com>
2019-05-15 01:08:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,6 @@ end
 def take_a_number(katz_deli,customer)
     if katz_deli == []
     katz_deli.each_with_index do |customer,index|
-    katz_deli<< "#{customer}#{index}"
     puts "Welcome, #{customer}. You are number #{index} in line."
   end
   end
