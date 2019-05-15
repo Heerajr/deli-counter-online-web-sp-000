de44abb2b9d9a2ecb61376b01e8bfba6b25d5eb9
Heeraj Ramsanai <ramsanai_heeraj@live.com>
2019-05-15 01:26:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,6 +19,6 @@ def take_a_number(katz_deli,customer)
   else
     new_customer = []
     new_customer<<katz_deli.push(customer)
-    puts "#{new_customer.join(",")}"
+    puts "Welcome, #{customer} . You are number #{new_customer.length} in line.
   end
 end
